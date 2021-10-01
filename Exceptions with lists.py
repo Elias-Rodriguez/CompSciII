d765e8cdbb2da19d3064e8e2abd80c2040121b95
Elias-Rodriguez <89213989+Elias-Rodriguez@users.noreply.github.com>
2021-10-01 12:29:38 -0500

    Add files via upload

---
+++ Exceptions with lists.py
@@ -0,0 +1,12 @@
+names = ['Ryley', 'Edan', 'Reagan', 'Henry', 'Caius', 'Jane', 'Guto', 'Sonya', 'Tyrese', 'Johnny']
+index = int(input())
+
+# Type your code here.
+try:
+    print('Name: {}'.format(names[index]))
+except IndexError as excpt:
+    print('Exception! {}'.format(excpt))
+    if index<0:
+        print("The closest name is: {}".format(names[0]))
+    else:
+        print("The closest name is: {}".format(names[len(names)-1]))
