6804c0b32a92257ea8bfa9e96009b039804c69ad
Elias-Rodriguez <89213989+Elias-Rodriguez@users.noreply.github.com>
2021-10-07 12:06:13 -0500

    Update Is Prime Function.py

--- Is Prime Function.py
+++ Is Prime Function.py
@@ -12,7 +12,7 @@ def prime_less_than(n):
     is_prime[0] = False
     is_prime[1] = False
     number = math.sqrt(n)
-    number = round(number)
+    number = round(number)+1
     for i in range(2, number):
         if is_prime[i]:
             for x in range(i * i, n, i):
@@ -21,4 +21,11 @@ def prime_less_than(n):
     return [i for i in range(n) if is_prime[i]]
 
 
-print(prime_less_than(10000))
+print("We are about to test how many numbers are prime that are less than the user given input.")
+user_num = int(input("What number would you like to use:\n"))
+print("Currently testing to see how many numbers are prime that are less than" , user_num)
+list_of_prime_numbers = prime_less_than(user_num)
+amount_of_prime_numbers = len(list_of_prime_numbers)
+print("There are {} prime numbers less than {}".format(amount_of_prime_numbers,user_num))
+print("Printing out the numbers found.")
+print(list_of_prime_numbers)
